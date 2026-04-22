b0bbbc8bfeef57612027c66b80c0657044a6ff0e
hego4545-hub <hego4545@gmail.com>
2026-04-22 03:20:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE_NAME='v5';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE_NAME='v10';
+self.addEventListener('install',e=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html']))); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>{if(k!==CACHE_NAME)return caches.delete(k);})))); self.clients.claim(); });
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+
