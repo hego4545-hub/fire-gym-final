8f929d4e2b21e1d8b96243bdbc6819c2a1c59f8d
hesham <hego4545@gmail.com>
2026-04-29 08:40:23 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-// SW.js - Cleared to resolve 500 errors and cache issues
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('fetch', (event) => {
-    // Direct network fetch, no caching
-    event.respondWith(fetch(event.request));
-});
